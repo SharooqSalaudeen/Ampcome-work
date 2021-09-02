661b9812978527b1b34c541847289a2a8153afc2
Sharooq <iamsharooq@gmail.com>
2021-09-02 13:01:35 +0530

    fixed errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import { Box } from "@material-ui/core";
 
-import Header from "../components/Header";
+// import Header from "../components/Header";
 import HomeSection1 from "../components/HomeSection1";
-import HomeSection1old from "../components/HomeSection1old";
-import HomeSection2old from "../components/HomeSection2old";
-import HomeSection3old from "../components/HomeSection3old";
-import HomeSection4old from "../components/HomeSection4old";
-import Footerold from "../components/Footerold";
+// import HomeSection1old from "../components/HomeSection1old";
+// import HomeSection2old from "../components/HomeSection2old";
+// import HomeSection3old from "../components/HomeSection3old";
+// import HomeSection4old from "../components/HomeSection4old";
+// import Footerold from "../components/Footerold";
 import Footer from "../components/Footer";
 
 import "../assets/styles/App.css";
